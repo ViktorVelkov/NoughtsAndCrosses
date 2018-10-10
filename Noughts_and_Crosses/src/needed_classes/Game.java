87fbe00cea540a7eb4aec21f6c7091402860cf56
ViktorVelkov <32453047+ViktorVelkov@users.noreply.github.com>
2018-10-10 21:32:48 +0300

    Just class Game.java

--- Noughts_and_Crosses/src/needed_classes/Game.java
+++ Noughts_and_Crosses/src/needed_classes/Game.java
@@ -161,13 +161,13 @@ public class Game {
         
         for ( int i = 0;i < 3;++i ) {
         	
-            s = s + "| ";
+            s = s + "| "; // 1
             
             for( int j = 0; j < 3; ++j) {
             	
                 if ( board[i][j] == null ) {
                 	
-                   s = s + "  | ";
+                   s = s + "  | "; // 1
                 
                 } else {
                 
